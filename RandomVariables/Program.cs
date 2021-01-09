affaa124321a6e897692302daf0b8e89821f0e0f
dashaka3 <dashaka3@gmail.com>
2021-01-09 12:42:37 +0500

    Начальный коммит

---
+++ RandomVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
